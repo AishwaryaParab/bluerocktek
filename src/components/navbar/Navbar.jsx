eba07d9564cd780cb44c20e85339be2e309f2329
Aishwarya Parab <aishwaryaparab1@gmail.com>
2025-06-08 10:09:30 +0530

    Changed branding of Bluerocktek

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,90 +1,151 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import styles from "./navbar.module.css"
-import logo from "../../../public/logo.png"
-import { poppins } from "@/utils/fonts"
-import { useContext, useEffect, useState } from "react"
-import { ScrollContext } from "@/context/ScrollContext"
-import { motion } from "framer-motion"
+import Image from "next/image";
+import styles from "./navbar.module.css";
+import logo from "../../../public/logo.png";
+import { poppins } from "@/utils/fonts";
+import { useContext, useEffect, useState } from "react";
+import { ScrollContext } from "@/context/ScrollContext";
+import { motion } from "framer-motion";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
-  const {homeRef, servicesRef, aboutRef, contactRef, scrollTo} = useContext(ScrollContext);
+  const { homeRef, servicesRef, aboutRef, contactRef, scrollTo } =
+    useContext(ScrollContext);
   const [isOpen, setIsOpen] = useState(false);
-//   const [header, setHeader] = useState(false);
+  //   const [header, setHeader] = useState(false);
 
-//   useEffect(() => {
-//     const changeBg = () => {
-//         if(window.scrollY >= 92) {
-//             setHeader(true);
-//         } else {
-//             setHeader(false);
-//         }
-//     }
+  //   useEffect(() => {
+  //     const changeBg = () => {
+  //         if(window.scrollY >= 92) {
+  //             setHeader(true);
+  //         } else {
+  //             setHeader(false);
+  //         }
+  //     }
 
-//     window.addEventListener('scroll', changeBg);
+  //     window.addEventListener('scroll', changeBg);
 
-//     return () => {
-//         window.removeEventListener("scroll", changeBg);
-//     };
-//   }, []);
+  //     return () => {
+  //         window.removeEventListener("scroll", changeBg);
+  //     };
+  //   }, []);
 
   const handleResponsiveMenu = () => {
-      setIsOpen(!isOpen);
-      // Enable/disable body scroll when the menu is open/closed
-      document.body.style.overflow = isOpen ? 'auto' : 'hidden';
+    setIsOpen(!isOpen);
+    // Enable/disable body scroll when the menu is open/closed
+    document.body.style.overflow = isOpen ? "auto" : "hidden";
   };
 
   const handleNavItemClick = (ref) => {
     handleResponsiveMenu();
     scrollTo(ref);
-  }
+  };
 
   return (
     <div>
-        <div ref={homeRef} className={styles.navbar}>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  transform: "translateX(-100%)",
-                  filter: "blur(5px)"
-                },
-                visible: {
-                  transform: "translateX(0)",
-                  filter: "blur(0)"
-                }
+      <div ref={homeRef} className={styles.navbar}>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              transform: "translateX(-100%)",
+              filter: "blur(5px)",
+            },
+            visible: {
+              transform: "translateX(0)",
+              filter: "blur(0)",
+            },
+          }}
+          className={styles.logo}
+        >
+          <Image
+            className={styles.logoImg}
+            src={logo}
+            width={260}
+            height={70}
+          />
+        </motion.div>
+
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: { transform: "translateX(100%)", filter: "blur(5px)" },
+            visible: { transform: "translateX(0)", filter: "blur(0)" },
+          }}
+          className={`${styles.links} ${poppins.className}`}
+        >
+          <ul>
+            <a
+              onClick={() => {
+                scrollTo(homeRef);
               }}
-              className={styles.logo}
             >
-                <Image className={styles.logoImg} src={logo} width={260} height={48} />
-            </motion.div>
+              <li>Home</li>
+            </a>
+            <a
+              onClick={() => {
+                scrollTo(servicesRef);
+              }}
+            >
+              <li>Services</li>
+            </a>
+            <a
+              onClick={() => {
+                scrollTo(aboutRef);
+              }}
+            >
+              <li>About Us</li>
+            </a>
+            <a
+              onClick={() => {
+                scrollTo(contactRef);
+              }}
+            >
+              <li>Contact Us</li>
+            </a>
+          </ul>
+        </motion.div>
 
-            <motion.div initial="hidden" animate="visible" variants={{hidden: {transform: "translateX(100%)", filter: "blur(5px)"}, visible: {transform: "translateX(0)", filter: "blur(0)"}}} className={`${styles.links} ${poppins.className}`}>
-                <ul>
-                    <a onClick={() => {scrollTo(homeRef)}}><li>Home</li></a>
-                    <a onClick={() => {scrollTo(servicesRef)}}><li>Services</li></a>
-                    <a onClick={() => {scrollTo(aboutRef)}}><li>About Us</li></a>
-                    <a onClick={() => {scrollTo(contactRef)}}><li>Contact Us</li></a>
-                </ul>
-            </motion.div>
+        <motion.div
+          onClick={handleResponsiveMenu}
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: { transform: "translateX(100%)", filter: "blur(5px)" },
+            visible: { transform: "translateX(0)", filter: "blur(0)" },
+          }}
+          className={styles.hamburgerIcon}
+        >
+          {isOpen ? <IoClose size={24} /> : <RxHamburgerMenu size={24} />}
+        </motion.div>
 
-            <motion.div onClick={handleResponsiveMenu} initial="hidden" animate="visible" variants={{hidden: {transform: "translateX(100%)", filter: "blur(5px)"}, visible: {transform: "translateX(0)", filter: "blur(0)"}}} className={styles.hamburgerIcon} >
-                {isOpen ? <IoClose size={24} /> : <RxHamburgerMenu size={24} />}
-            </motion.div>
-
-            <div className={isOpen ? `${styles.navItemsMobile} ${styles.active}` : `${styles.navItemsMobile}`}>
-                <a onClick={() => handleNavItemClick(homeRef)}><li>Home</li></a>
-                <a onClick={() => handleNavItemClick(servicesRef)}><li>Services</li></a>
-                <a onClick={() => handleNavItemClick(aboutRef)}><li>About Us</li></a>
-                <a onClick={() => handleNavItemClick(contactRef)}><li>Contact Us</li></a>
-            </div>
+        <div
+          className={
+            isOpen
+              ? `${styles.navItemsMobile} ${styles.active}`
+              : `${styles.navItemsMobile}`
+          }
+        >
+          <a onClick={() => handleNavItemClick(homeRef)}>
+            <li>Home</li>
+          </a>
+          <a onClick={() => handleNavItemClick(servicesRef)}>
+            <li>Services</li>
+          </a>
+          <a onClick={() => handleNavItemClick(aboutRef)}>
+            <li>About Us</li>
+          </a>
+          <a onClick={() => handleNavItemClick(contactRef)}>
+            <li>Contact Us</li>
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
